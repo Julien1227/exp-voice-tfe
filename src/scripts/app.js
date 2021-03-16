4e0ee950e0af7d8fed42086da8c809ae36e0b9b3
Julien <julien19991227@gmail.com>
2021-03-16 20:31:20 +0100

    Transfer pour mon portable

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -23,23 +23,34 @@ function createAnalyserNode(audioSource) {
   audioSource.connect(analyserNode);
 }
 
+const color1 = document.querySelector('.color1');
+const color2 = document.querySelector('.color2');
+const span1 = document.getElementById('span-1');
+const span2 = document.getElementById('span-2');
+const span3 = document.getElementById('span-3');
+
+
 // get's html elements, loops over them & attaches a frequency from analysed data - what you do is up to you!
 function animateStuff() {
   requestAnimationFrame(animateStuff);
   analyserNode.getByteFrequencyData(frequencyData);
-  
+
   // Animation stuff--------------------------------
   var allRepeatedEls = document.getElementsByTagName('i');
+  //allRepeatedEls = selectorAll des i
   var totalEls = allRepeatedEls.length;
+  //totalEls = nombre de "i"
 
-  // Simple example of changing opacity & colour -> EDIT THIS!
+  // assigne une fréquence à chaque i, lorsque la fréquence est supérieure à 100, elle est considérée comme jouée et le block se lumine
   for (var i=0; i<totalEls; i++) {
     // set colours
-    var lightColour = i*10;
+    var lightColour = i*3;
     allRepeatedEls[i].style.backgroundColor = 'hsla('+lightColour+',  80%, 50%, 0.8)';
     allRepeatedEls[i].style.borderColor = 'hsla('+lightColour+',  80%, 50%, 1)';
+
     // flash on frequency
-    var freqDataKey = i*2;
+    var freqDataKey = i * 3;
+
     if (frequencyData[freqDataKey] > 100){
       // frequency played - make opache
       allRepeatedEls[i].style.opacity = "1";
@@ -47,7 +58,22 @@ function animateStuff() {
       // frequency not played - fade out
       allRepeatedEls[i].style.opacity = "0.2";
     }
+    console.log(frequencyData[1]);
+    color2.style.backgroundColor = 'hsl('+frequencyData[freqDataKey]*3.6+',  80%, 50%, 0.8)';
+    color1.style.backgroundColor = 'rgba('+frequencyData[0]*3+', '+frequencyData[200]*3+', '+frequencyData[450]*3+', 1)';
+    span.innerHTML = frequencyData[200];
+    
   }
+
+  /* COMMENTAIRE - COMMENT CA FONCTIONNE
+
+  frequencyData[0] = fréquence graves
+  frequencyData[200-400+-] = fréquence moyennes
+  frequencyData[400+] = fréquence très aigues
+  Plus le brui est fort, plus les nombres enregistré à ces fréquences sont grand
+
+  */
+
 }
 
 // getUserMedia success callback -> pipe audio stream into audio API
